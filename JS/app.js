787c63f138753d180858a52d687be5db03ded49c
Adebanjo Muhammed <sayhello2moa@gmail.com>
2022-03-14 15:49:08 +0100

    My Portfolio First Commit

---
+++ JS/app.js
@@ -0,0 +1,34 @@
+const hamburger = document.querySelector(".header .nav-bar .nav-links .hamburger");
+const mobile_menu = document.querySelector(".header .nav-bar .nav-links ul");
+const menu_item = document.querySelectorAll(".header .nav-bar .nav-links ul li a");
+const header = document.querySelector(".header.container");
+
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  mobile_menu.classList.toggle("active");
+});
+
+document.addEventListener("scroll", () => {
+  const scroll_position = window.scrollY;
+  if (scroll_position > 2) {
+    header.style.backgroundColor = "#151D3B";
+  } else {
+    header.style.backgroundColor = "transparent";
+  }
+});
+
+menu_item.forEach((item) => {
+  item.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobile_menu.classList.toggle("active");
+  });
+});
+
+let typed = new Typed(".typing", {
+  strings: [ "Web Designer.", "Frontend Developer.", "JavaScript Developer." ],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true
+});
+
