58ba7f9c9985edd94b97f62757d77f53cc02ea9e
Adebanjo Muhammed <sayhello2moa@gmail.com>
2022-04-20 20:39:31 +0100

    css modified/dark theme added

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -1,40 +1,69 @@
-// Header / Hamburger section
+// Header | Hamburger section
 const hamburger = document.querySelector(".header .nav_bar .nav_links .hamburger");
 const mobile_menu = document.querySelector(".header .nav_bar .nav_links ul");
 const menu_item = document.querySelectorAll(".header .nav_bar .nav_links ul li a");
 const header = document.querySelector(".header.container");
 
 hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  mobile_menu.classList.toggle("active");
+	hamburger.classList.toggle("active");
+	mobile_menu.classList.toggle("active");
 });
 
-document.addEventListener("scroll", () => {
-  const scroll_position = window.scrollY;
-  if (scroll_position > 2) {
-    header.style.backgroundColor = "#151D3B";
-  } else {
-    header.style.backgroundColor = "transparent";
-  }
-});
+
 
 menu_item.forEach((item) => {
-  item.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    mobile_menu.classList.toggle("active");
-  });
+	item.addEventListener("click", () => {
+	hamburger.classList.toggle("active");
+	mobile_menu.classList.toggle("active");
+	});
+});
+
+
+
+document.addEventListener("scroll", () => {
+	const scroll_position = window.scrollY;
+	if (scroll_position > 2) {
+	header.style.backgroundColor = "#151D3B";
+	} else {
+	header.style.backgroundColor = "transparent";
+	}
 });
 // End of Header / Hamburger section
 
 
 // Typing Section
 let typed = new Typed(".typing", {
-  strings: ["Frontend Developer.", "JavaScript Developer." ],
-  typeSpeed: 100,
-  backSpeed: 60,
-  loop: true
+	strings: ["Frontend Developer.", "JavaScript Developer." ],
+	typeSpeed: 100,
+	backSpeed: 60,
+	loop: true
 });
 // End of Typing Section
+
+
+// Animate On Scroll
+	AOS.init({
+	offSet: 120,
+	duration: 1500,
+	dataAosEasing: "ease-in-out",
+	});
+// End of Animate On Scroll
+
+
+// Dark Theme
+const darkIcon = document.getElementById("icon");
+
+darkIcon.addEventListener("click", () => {
+	document.body.classList.toggle("dark_theme");
+
+	if (document.body.classList.contains("dark_theme")) {
+		darkIcon.src = "Assets/Img/sun.png";
+	} else {
+		darkIcon.src = "Assets/Img/moon.png";
+	}
+});
+
+
 
 
 // Read More Section
@@ -43,7 +72,7 @@ let typed = new Typed(".typing", {
 //   const dots = document.getElementById("dots");
 //   const moreText = document.getElementById("more");
 //   const button = document.getElementById("myBtn");
-  
+
 //   if (dots.style.display === "none") {
 //     dots.style.display = "inline";
 //     button.innerHTML = "Read More";
@@ -56,11 +85,3 @@ let typed = new Typed(".typing", {
 // });
 // End of Read More Section
 
-
-// Animate On Scroll
-  AOS.init({
-    offSet: 120,
-    duration: 1500,
-    dataAosEasing: "ease-in-out",
-  });
-// End of Animate On Scroll
